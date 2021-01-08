c2933072155a75054948e28a7a6909aa588843c7
Domenicaacosta <acostadomenica30@gmail.com>
2021-01-08 16:28:37 -0500

    WS01 Hellow World int three programming languages

---
+++ workshops/unit1/WS01helloWorld/helloWorld 1.java
@@ -0,0 +1,12 @@
+// Domenica Acosta Hello World
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class HelloWorld{
+    public static void main(String[] args){
+		System.out.println("Hello World from Domenica Acosta");
+		}
+	}
